8785cfe4c3d549dc4b9b5036d0a77e77bdb21d9f
Nisha <nisha@Jenishas-MacBook-Air.local>
2025-04-27 14:38:53 +0545

    bye

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import "./data/places.json"
 import './App.css'
+import SignUp from './pages/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <SignUp/>
+      <Footer/>
     </>
   )
 }
