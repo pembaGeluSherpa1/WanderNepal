3825b8e8fd3302613334f9df16124d32f92979ff
mingSherpa69 <mingmar.sherpa@aadimcollege.edu.np>
2025-04-27 14:37:46 +0545

    Group work day 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import "./data/places.json"
 import './App.css'
 import Login from './Pages/Login'
+import VisitList from './component/VisitList'
+import Testimonial from './component/Testimonial'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <Login/>
+      <VisitList/>
+      <Testimonial/>
     </>
   )
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,23 @@
 import "../assets/Login.css"
+import { CiLock , CiMail} from "react-icons/ci";
+
 function Login(){
     return(
         <>
-        <div className="container">
+        <img src="https://img.freepik.com/free-photo/vertical-shot-lochawa-la-khang-monastery-kalpa-himachal-pradesh-cold-winter_181624-23562.jpg?semt=ais_hybrid&w=740" alt="Background" id="Background-img"/>
+        <div className="Login-container">
             <form action="">
                 <h1>Login</h1>
+                <p>Your gateway to Wander Nepal Pass</p>
+
                 <div className="user">
                 <label><input type="Username" placeholder="Username" /></label>
+                <CiMail />
                 </div>
 
                 <div className="psw">
                 <label><input type="Password" placeholder="Password"/></label>
+                <CiLock />
                 </div>
 
                 <button type="Submit">Login</button>
@@ -18,6 +25,11 @@ function Login(){
                 <div className="remember-forget">
                     <p>Don't remember password?<a href="#">Forgot Password</a></p>
                 </div>
+
+            
+            <div className="signn">
+                <p>Need to create an account?<a href="#">Sign-up</a></p>
+            </div>
 
             </form>
         </div>
